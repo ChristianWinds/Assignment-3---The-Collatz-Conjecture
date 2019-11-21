34310edcfe97c518c1d111a72eac9c7e481f25e8
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-21 11:29:56 -0500

    Initial upload

---
+++ c++/collatz.cpp
@@ -0,0 +1,23 @@
+/* Prgrammer: Christian Winds
+ * Course: CSC 330 Organization of Programming Languages
+ * School Year: 2019-2020 School Year */
+
+using namespace std;
+
+// This program employs a "CollatzInfo" class type to pair integers with the
+// integers' respective Collatz sequence lengths
+class CollatzInfo
+{
+	public:
+
+	int integer;
+
+	// Create an integer variable to store an integer's Collatz sequence
+	// length
+	int collatzLength;
+}
+
+int main()
+{
+	
+}
