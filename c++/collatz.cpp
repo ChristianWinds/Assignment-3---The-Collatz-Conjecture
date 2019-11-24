7952f8ed38e19acf4e04f1363ddeb45fd1cee609
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-24 15:45:38 -0500

    Updated

--- c++/collatz.cpp
+++ c++/collatz.cpp
@@ -21,13 +21,52 @@ class CollatzInfo
 
 int main()
 {
-	storeCollatzInfo();
+
+	const int maxCollatzLength = 5000000000;
+	storeCollatzInfo(maxCollatzLength);
 }
 
-void storeCollatzInfo()
+void storeCollatzInfo(int maxCollatzLength)
 {
 	// Precondition:
 	// Postcondition:
 
-	;
+	
+
+	for (int integer = 1; integer <= INT_MAX; integer++)
+	{
+		calculateCollatzLength(integer, maxCollatzLength);
+	}
+}
+
+int calculateCollatzLength(int integer, int maxCollatzLength)
+{
+	// Precondition: Both the received integer to be evaluated and the
+	// received maximum Collatz sequence length were valid positive integers
+	// Postcondition: The Collatz length calculated for the received integer
+	// was returned to this function's caller if the length was less than or
+	// equal to the maximum Collatz length parameter, or a negative one was
+	// returned to this function's caller if the calculated Collatz length
+	// exceeded the maximum Collatz length parameter
+
+	int collatzLength = 0;
+
+	while ((integer != 1) &&
+	       (collatzLength < maxCollatzLength))
+	{
+		// Check whether the integer is even or odd to determine which
+		// Collatz Conjecture operations should be performed
+		if (integer % 2 == 0)
+		{
+			integer = integer / 2;
+		}
+		else
+		{
+			integer = (integer * 3) + 1;
+		}
+
+		collatzLength++;
+	}
+
+	return collatzLength;
 }
