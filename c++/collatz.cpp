f33f0e11372fc68fb440c8d58b656cddd0424845
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-25 09:56:58 -0500

    Changed for testing

--- c++/collatz.cpp
+++ c++/collatz.cpp
@@ -46,7 +46,7 @@ int main()
 
 	// Create an integer constant to designate the maximum integer to receive
 	// a Collatz sequence length calculation
-	const int maxIntToEvaluate = INT_MAX;
+	const int maxIntToEvaluate = 5;
 
 	// Calculate the Collatz sequence lengths of the positive primitive
 	// integer range to determine the longest Collatz sequence lengths
