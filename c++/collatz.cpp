7ed4b7bc0414ee2eb503b76de04a482f7b5d82d2
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-25 15:59:55 -0500

    Corrected

--- c++/collatz.cpp
+++ c++/collatz.cpp
@@ -199,7 +199,7 @@ void printByCollatzLengths(multimap<long int, int> collatzLengthMap)
 
 	const string leftColumnName = "Collatz Sequence Length";
 	const string rightColumnName = "Integer";
-	const string separator = " | "
+	const string separator = " | ";
 
 	// Print a header to label the Collatz sequence length and evaluated
 	// integer columns
