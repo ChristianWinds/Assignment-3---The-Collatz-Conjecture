0d91946ecb58c556a667c6ac29e5384ea5576b94
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-29 13:45:47 -0500

    Updated

--- java/Collatz.java
+++ java/Collatz.java
@@ -2,9 +2,11 @@
  * Course: CSC 330 Organization of Programming Languages
  * School Year: 2019-2020 School Year */
 
+import javafx.util.Pair;
+
 public class Collatz
 {
-	static void storeCollatzInfo()
+	static void storeCollatzInfo(int maxIntToEvaluate)
 	{
 		// Precondition:
 		// Postcondition:
@@ -13,6 +15,19 @@ public class Collatz
 		     integer <= maxIntToEvaluate;
 		     integer++)
 		{
+			int collatzLength = calculateCollatzLength(intToEvaluate,
+								   maxCollatzLength);
+
+			// Check if the current integer's Collatz sequence
+			// length is greater than zero to prevent storage of
+			// invalid Collatz sequence lengths
+			if (collatzLength > 0)
+			{
+				// Check the number of elements in the Collatz
+				// Length multimap to retain only the specified
+				// quantity of longest Collatz sequence length
+				// and integer pairs
+
 			// End the Collatz sequence length calculations if the
 			// maximum specified integer of the integer calculation
 			// range was met to avoid an error
