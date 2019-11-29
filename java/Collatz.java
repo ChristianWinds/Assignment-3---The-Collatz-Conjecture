721eea57b04745ca0c0d713d88782fa7853b2029
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-29 13:10:27 -0500

    Renamed collatz.java to Collatz.java

---
+++ java/Collatz.java
@@ -0,0 +1,11 @@
+/* Programmer: Christian Winds
+ * Course: CSC 330 Organization of Programming Languages
+ * School Year: 2019-2020 School Year */
+
+class Collatz
+{
+	public static void main()
+	{
+		
+	}
+}
