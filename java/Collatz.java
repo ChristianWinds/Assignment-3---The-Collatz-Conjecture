793d15faaae1abbcce0472f081be73f9b475bb00
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-29 13:19:48 -0500

    Updated

--- java/Collatz.java
+++ java/Collatz.java
@@ -2,10 +2,19 @@
  * Course: CSC 330 Organization of Programming Languages
  * School Year: 2019-2020 School Year */
 
-class Collatz
+public class Collatz
 {
+	static void storeCollatzInfo()
+	{
+		// Precondition:
+		// Postcondition:
+	}
+
 	public static void main()
 	{
-		
+		// Calculate the Collatz sequence lengths of the positive
+		// primitive integer range to determine the longest Collatz
+		// sequence lengths
+		storeCollatzInfo();
 	}
 }
