aa9ed91c3d574df32f522d8ad1821e15d3d45211
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-11-30 15:33:04 -0500

    Updated

--- java/Collatz.java
+++ java/Collatz.java
@@ -12,12 +12,16 @@ public class Collatz
 		// Precondition:
 		// Postcondition:
 
-		for (long integer = 1;
-		     integer <= maxIntToEvaluate;
-		     integer++)
+		// Create an integer variable to track the number of items in
+		// the Collatz length array
+		int arrayQuantity = 0;
+
+		for (long intToEvaluate = 1;
+		     intToEvaluate <= maxIntToEvaluate;
+		     intToEvaluate++)
 		{
-			int collatzLength = calculateCollatzLength(intToEvaluate,
-								   maxCollatzLength);
+			long collatzLength = calculateCollatzLength(intToEvaluate,
+								    maxCollatzLength);
 
 			// Check if the current integer's Collatz sequence
 			// length is greater than zero to prevent storage of
@@ -25,9 +29,17 @@ public class Collatz
 			if (collatzLength > 0)
 			{
 				// Check the number of elements in the Collatz
-				// Length multimap to retain only the specified
+				// length array to retain only the specified
 				// quantity of longest Collatz sequence length
 				// and integer pairs
+				if (arrayQuantity < collatzLengthsToStore)
+				{
+					putInDescendingArray();
+				}
+				else
+				{
+					putInDescendingFullArray();
+				}
 
 			// End the Collatz sequence length calculations if the
 			// maximum specified integer of the integer calculation
@@ -39,8 +51,8 @@ public class Collatz
 		}
 	}
 
-	static void putInArraySortDescending (Pair <Long, Long> collatzPair,
-					      Pair <Long, Long> [] collatzArray)
+	static void putInDescendingArray (Pair <Long, Long> collatzPair,
+					  Pair <Long, Long> [] collatzArray)
 	{
 		// Precondition:
 		// Postcondition:
@@ -60,6 +72,20 @@ public class Collatz
 
 	public static void main()
 	{
+		// Create an integer variable to determine the number of Collatz
+		// sequence lengths to store
+		int collatzLengthsToStore = 10;
+
+		Pair <Long, Long> [] collatzLengthArray = new Pair <Long, Long>[collatzLengthsToStore];
+
+		// Create a long variable to determine the maximum permitted
+		// Collatz sequence length to store
+		long maxCollatzLength = 5000000000L;
+
+		// Create a long variable to designate the maximum integer to
+		// receive a Collatz sequence length calculation
+		long maxIntToEvaluate = Long.MAX_VALUE;
+
 		// Calculate the Collatz sequence lengths of the positive
 		// primitive integer range to determine the longest Collatz
 		// sequence lengths
