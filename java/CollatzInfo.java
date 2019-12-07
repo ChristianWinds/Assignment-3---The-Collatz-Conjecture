20d7dde4005c6fef65630b1c90e4ecc3fe119f42
Christian T. Winds <winds_ct@cobra.cs.mercer.edu>
2019-12-06 20:50:02 -0500

    Upload

---
+++ java/CollatzInfo.java
@@ -0,0 +1,8 @@
+/* Programmer: Christian Winds
+ * Course: CSC 330 Organization of Programming Languages
+ * School Year: 2019-2020 School Year */
+public class CollatzInfo
+{
+	long collatzLength;
+	long integer;
+}
